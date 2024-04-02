3c6af489ebccc43f1dcbec994f08477744e44aa5
Aeyoz <elpayov2@gmail.com>
2024-04-02 12:30:41 +0100

    contact form

---
+++ angular-light/projects/sites/www/src/app/pages/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [MdbFormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+}
