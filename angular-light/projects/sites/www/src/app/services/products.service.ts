874a235f202fd1cce656f48283c1bf16c0ac9e49
Aeyoz <elpayov2@gmail.com>
2024-04-05 12:25:04 +0100

    product service created

---
+++ angular-light/projects/sites/www/src/app/services/products.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { products } from '../../assets/db/products';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetproductsService {
+
+  constructor() { }
+
+  getProducts() {
+    return products
+  }
+
+  getProduct(productId: number) {
+    return products.find((product)=> productId === product.id)
+  }
+
+  getProductCategories(productId: number): Array<string> | void {
+    for (let product of products) {
+      if (product.id === productId) {
+        return product.categorias
+      }
+    }
+  }
+
+  getProductTags(productId: number): Array<string> | void{
+    for (let product of products) {
+      if (product.id === productId) {
+        return product.etiquetas
+      }
+    }
+  }
+
+  getProductPrice(productId: number): Number {
+    let product = products.find((product)=> productId === product.id)
+    return product.precioRebajado ? product.precioRebajado: product.precio
+  }
+
+}
